57f27b7f665b1a47857a50991dbf98716f3fcc3e
Adrian Lopez <adrianlzt@gmail.com>
2013-12-14 21:18:34 +0100

    scripts para analizar logs de apache y ocupacion de directorios

---
+++ apache_logs/log-apache-to_pdf.r
@@ -0,0 +1,64 @@
+#!/usr/bin/env Rscript
+#
+# Obtener estadisticas de fichero access_log
+#
+# El formato del fichero de log debe ser:
+# 1.2.4.5 - - [08/Dec/2013:03:19:11 +0100] "POST /url HTTP/1.1" 500 20 "http://1.2.4.5/" "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0"
+#
+# El parseo de la fecha esta puesto para idioma ingles (LC_TIME=C). Cambiar si es necesario (por ejemplo si el mes es "Dic")
+#
+# Uso: Rscript log-apache.r /var/log/httpd/access_log
+#
+# Gracias a http://data-experience.blogspot.de/
+#
+
+# Cargo libreria necesaria
+library(treemap)
+
+# Obtengo los parametros donde apunta al fichero de log
+args <- commandArgs(trailingOnly = TRUE)
+
+# Leo el fichero y lo convierto a un data.frame
+ac <- read.table(args[1])
+
+# Le damos nombres a las columnas
+colnames(ac)=c("host",NA,NA,"date","timezone","request","status","bytes","url","browser")
+
+# Quitamos las columnas que no queremos (las dos vacias y el timezone)
+ac <- ac[c("host","date","request","status","bytes","url","browser")]
+
+# Convertir la fecha, mirar date.md
+NUL <- Sys.setlocale("LC_TIME", "C") 
+ac$date <- strptime(ac$date,format('[%d/%b/%Y:%H:%M:%S'))
+
+# Campo para poder pintar el treemap
+ac$count <- c(1)
+
+# Add day, hour and year-month column
+ac$day = format(ac$date,'%A')
+ac$hour = format(ac$date,'%H')
+ac$yearmonth = format(ac$date, "%Y-%m")
+
+# Mostrar todas las graficas en una misma ventana
+par(mfrow=c(2,2))
+
+# Graficas con numero de peticiones por year-month 
+barplot(table(ac$yearmonth), xlab="Year-Month", ylab="Num peticiones")
+
+# Graficas con numero de peticiones por hora
+barplot(table(ac$hour), xlab="Hour", ylab="Num peticiones")
+
+# Graficas con numero de peticiones por dia de la semana
+barplot(table(factor(ac$day, levels=c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))), xlab="Day", ylab="Num peticiones")
+
+# Número de peticiones por cada código de contestación (200,405,500...)
+barplot(table(ac$status),log="y",xlab="Codigos HTTP",ylab="Num peticiones (Log)")
+
+# Mostrar hosts que más acceden en un formato de cajas, donde a mayor tamaño más accesos, en una ventana nueva
+dev.new()
+treemap(ac, index="host", vSize='count',title="Clientes")
+
+# Nos devuelve las filas que tienen ".*json.*" en la columna request
+# ac[grep('.*json.*',ac$request),]
+
+cat("Graficas generadas en Rplots.pdf y Rplots1.pdf\n")
---
+++ ocupacion_filesystem/ocupacion-filesystem.r
@@ -0,0 +1,29 @@
+# Cojo los parametros
+args <- commandArgs(trailingOnly = TRUE)
+
+# Cargo las librerias necesarias
+library(treemap)
+library(tools)
+
+# Ejecuto ls y meto la salida en la variable 'ls'
+exec <- paste(c("ls -l --time-style=+%Y-%m-%d:%H:%M:%S",args),collapse=' ')
+ls <- system(exec, intern=T,ignore.stderr=T)
+
+# Quito las cabeceras de ls (total y nombre del directorio, si se pasa mas de uno)
+ls <- grep('[d-]{1}.*',ls,value=T)
+
+# Convierto 'ls' en un date.frame
+dfls <- read.table(textConnection(ls))
+
+# Pongo los nombres de las columnas
+colnames(dfls) = c("perms","links","user","group","size","date","file")
+
+# Convierto la fecha a R
+dfls$date <- strptime(dfls$date,format('%Y-%m-%d:%H:%M:%S'))
+
+# Introduzco columna con las extensiones de los ficheros
+dfls$ext <- file_ext(dfls$file)
+
+# Abro el navegador para ver el tree map
+itreemap(dfls,index="file",vSize="size")
+
